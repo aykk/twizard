eed3df3fda16ff464f78b4f90f4e4361552e2ebd
Corvails <86210175+Corvails@users.noreply.github.com>
2022-11-12 08:03:36 -0500

    more improvements

--- src/App.js
+++ src/App.js
@@ -1,62 +1,64 @@
 import React, { Component } from 'react';
+import  Elements  from './Elements';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-import { render } from "react-dom";
+import './style2.css';
 
 const mapStyles = {
-  width: '750px',
-  height: '350px',
-  margin: 310,
-  padding: 30,
+  width: '50%x',
+  height: '50%',
+  margin: 50,
+  padding: 0
 };
 
 var lati = 0;
 var longi = 0;
 
-class App extends Component {
+export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
     };
   }
+  
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      lati = position.coords.latitude;
+      longi = position.coords.longitude;
+    });
+  }
 
-componentDidMount() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    lati = position.coords.latitude;
-    longi = position.coords.longitude;
-  });
-}
-
-render() {
-  return (
-    <div>
-    </div>
-  );
-}
-}
-
-render(<App />, document.getElementById("root"));
-
-export class MapContainer extends Component {
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={20}
-        style={mapStyles}
-        initialCenter={
-          {
-            lat: lati,
-            lng: longi,
+      <div>
+        <h1>TinkleWizard</h1> 
+        <p>Need to pee? Tinkle Wizard has got your back!</p>
+        <Elements/>
+        <p2>(How badly do you need to go?)</p2>
+        <br/>
+        <p><button id = "button">Tinkle</button> </p>
+        <br/>
+        <p3> Risk: 98%  </p3>
+        <br/>
+        <p3>Location: 180 Kent Street</p3>
+        <Map
+            google={this.props.google}
+            zoom={20}
+            style={mapStyles}
+            initialCenter={
+            {
+              lat: 37.230358,
+              lng: -80.422379
+            }
           }
-        }
-      >
-        <Marker
-        />
-      </Map>
+        >
+          <Marker
+          />
+        </Map>
+      </div>
     );
   }
 }
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCT3OIk64RCKckk0m05jl9ZnyFP9OoaPY0'
-})(MapContainer);
+})(App);
