27b0331c04a7c259a5b24b88e5578903a690e885
Corvails <86210175+Corvails@users.noreply.github.com>
2022-11-12 18:52:42 -0500

    added button and slider functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { render } from "react-dom";
 import  Elements  from './Elements';
+import  { returnUrgency }  from './Elements';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import './style2.css';
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import "./App.css"; 
+
+const googleApi = "https://maps.googleapis.com/maps/api/place/search/json?location=";
+const googleApiEnd = "&keyword=place&key=AIzaSyCT3OIk64RCKckk0m05jl9ZnyFP9OoaPY0";
+
+var urgency = 0;
+var radius = 110 - urgency;
+var latiNew = 0;
+var longiNew = 0;
+
+async function getPeeLocation(){
+  urgency = returnUrgency();
+  alert(urgency);
+  const fullLink = googleApi + lati + "," + longi + "&radius=" + radius + googleApiEnd;
+  latiNew = fullLink;
+  longiNew = fullLink;
+ alert("pepepepep");
+
+}
 
 const mapStyles = {
   width: '50%x',
-  height: '50%',
+  height: '55%',
   margin: 50,
   padding: 0
 };
@@ -19,7 +42,7 @@ export class App extends Component {
     this.state = {
     };
   }
-  
+
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(function(position) {
       lati = position.coords.latitude;
@@ -35,9 +58,11 @@ export class App extends Component {
         <Elements/>
         <p2>(How badly do you need to go?)</p2>
         <br/>
-        <p><button id = "button">Tinkle</button> </p>
+        <Col>
+            <Button onClick={(e) => getPeeLocation()}> Tinkle </Button>
+        </Col>
         <br/>
-        <p3> Risk: 98%  </p3>
+        <p3> Risk: +   </p3>
         <br/>
         <p3>Location: 180 Kent Street</p3>
         <Map
@@ -46,8 +71,8 @@ export class App extends Component {
             style={mapStyles}
             initialCenter={
             {
-              lat: 37.230358,
-              lng: -80.422379
+              lat: lati,
+              lng: longi
             }
           }
         >
@@ -58,6 +83,8 @@ export class App extends Component {
     );
   }
 }
+
+render(<App />, document.getElementById("root"));
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCT3OIk64RCKckk0m05jl9ZnyFP9OoaPY0'
--- src/elements.js
+++ src/elements.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import './style2.css';
+import Col from "react-bootstrap/Col";
 
+var value = 0;
 
+export function returnUrgency(){
+  return value;
+}
 
 export default function Elements() {
   return (
     <div class="slidecontainer">
-        <input type="range" min="1" max="100" class="slider" id="myRange"/>
+      <Col>
+        <input onChange = {(e)=> (value = (e.target.value))} type="range" min="1" max="100" class="slider" id="myRange"/>
+      </Col>
+        
     </div>
+    
   )
 }
+
+
