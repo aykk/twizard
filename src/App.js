6678daea85e5097bd3bbacabfc84712c3782beda
ayk <jungmink623@gmail.com>
2022-11-16 21:02:17 -0500

    v1.0 complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from "react-dom";
-import  Elements  from './Elements';
-import  { returnUrgency }  from './Elements';
+import  Elements  from './elements';
+import  { returnUrgency }  from './elements';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import './style2.css';
 import "./App.css"; 
@@ -19,23 +19,27 @@ async function getPeeLink(){
   urgency = returnUrgency();
   radius = Math.floor(110 - urgency);
   fullLink = googleApi + lati + "," + longi + "&radius=" + radius + googleApiEnd;
-  alert("There is a location to piss within a " + radius + " foot radius!");
+  alert("There is a location to use the bathroom within a " + radius + " foot radius!");
   alert(fullLink);
   const element = document.getElementById("radi");
   element.innerHTML = "Radius:" + radius + " feet";
+  const element2 = document.getElementById("link");
+  element2.innerHTML = "Link: " + fullLink;
 }
 
 const mapStyles = {
   width: '50%x',
   height: '50%',
   margin: 50,
-  padding: 0
+  padding: 0,
+  border: '4px solid black',
+  borderRadius: '10px'
 };
 
 var lati = 0;
 var longi = 0;
 
-var message = "You are 100% likely to get caught if you piss here! Find a bathroom instead!";
+var message = "You are 100% likely to get caught if you go here! Find a bathroom instead!";
 
 export class App extends Component {
   constructor(props) {
@@ -64,12 +68,14 @@ export class App extends Component {
             <Button onClick={(e) => getPeeLink()}> Tinkle </Button>
         </Col>
         <br/>
-        <p3 id = "radi"> Radius: {radius} feet</p3>
+        <p3 id = "radi"><b>Radius:</b> {radius} feet</p3>
         <br/>
-        <p3>Current Location: {lati}, {longi} (Holden Hall)</p3>
+        <p3><b>Current Location:</b> {lati}, {longi}</p3>
+        <br/>
+        <p3 id = "link"> <b>Link:</b> {fullLink}</p3>
         <br/>
         <br/>
-        <p3><b><i>{message}</i></b></p3>
+        <p3 class = "msg"><b><i>{message}</i></b></p3>
         <Map
             google={this.props.google}
             zoom={20}
